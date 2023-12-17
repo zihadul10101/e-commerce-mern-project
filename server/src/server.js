f5fcf9748f0a04ae1f162604137251afccafd5d2
zihad <zihadul10101@gmail.com>
2023-12-17 11:00:09 +0600

    Third-party middleware use

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
 
 
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended:true}));
 const port = 3000
 const isLoggedIn=(req,res,next)=>{
   // console.log("Is loggedin middlewar");
@@ -33,7 +36,19 @@ app.get('/api/user',isLoggedIn, (req, res) => {
     message:"user profile return"
   })
 })
+// client error handling error
+app.use(( req, res, next) => {
 
+  res.status(404).json({
+    message:"route not found"
+  });
+  next();
+})
+// server error handling error
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
