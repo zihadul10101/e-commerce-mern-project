04da1cf4825bbd549639a6f87597b745220e3132
zihad <zihadul10101@gmail.com>
2024-01-26 08:50:08 +0600

    category modeal create

---
+++ server/src/models/categoryModels.js
@@ -0,0 +1,24 @@
+const {Schema,model} = require('mongoose');
+
+
+const categorySchema= new Schema({
+  name:{
+   type:  String,
+   require:[true,'category name is required'],
+   trim:true,
+   unique:true,
+   minLength:[3,"The length of category name can be minmum 3"],
+  },
+  slug:{
+   type:  String,
+   require:[true,'category slug is required'],
+   lowercase:true,
+   unique:true,
+  },
+ 
+ 
+},{timestamps: true});
+
+
+const Category=model('Category',categorySchema);
+module.exports= Category;
